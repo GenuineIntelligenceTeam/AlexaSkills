2e55b8b67b76439fd9cca24d9783adc1e0e04cce
Ashley Teng <ashlaeteng@dhcp-18-111-14-65.dyn.MIT.EDU>
2018-08-02 15:32:35 -0400

    Alexa Skill Study Mode

---
+++ study_mode.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_ask import Ask, statement, question
+import requests
+import time
+import unidecode
+import json
+import random
+
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@app.route('/')
+def homepage():
+    return "Hello"
+
+@ask.launch
+def start_skill():
+    welcome_message = 'Please state the status'
+    return question(welcome_message)
+
+
+@ask.intent("loud")
+def loud():
+    loud = 'It sounds like the room is too loud. Go find a quieter place to study.'
+    return statement(loud)
+
+@ask.intent("distracting")
+def distracting():
+    output = 'It sounds like people are chatting. Stay focused.'
+    return statement(output)
+
+@ask.intent("sleep")
+def sleep():
+    wake_up = 'Hello there, please wake up, we\'re on study mode!'
+    return statement(wake_up)
+
+@ask.intent("StopIntent")
+def sleep():
+    bye_text = 'Study Mode turned off'
+    return statement(bye_text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
